c9ed8b5011edb7fd3673f1412e669a0ec2947c7d
SeanEArc <seanearcaya@gmail.com>
2025-09-12 17:46:20 -0500

    Added 125_Valid_Palindrome.java. Attempting to solve problems in Java from now on to increaase familirity with the language.

---
+++ solutions/125_Valid_Palindrome.java
@@ -0,0 +1,24 @@
+// 125. Valid Palindrome JAVA solution
+// https://leetcode.com/problems/valid-palindrome/
+
+class Solution {
+    public boolean isPalindrome(String s) {
+        
+        if (s == null){
+            return true;
+        }
+
+        String cleanedString = s.toLowerCase().replaceAll("[^a-z0-9]", "");
+
+        String reversedString = new StringBuilder(cleanedString).reverse().toString();
+
+        if (cleanedString.equals(reversedString)){
+            return true;
+        }
+
+        return false;
+
+        
+        
+    }
+}
