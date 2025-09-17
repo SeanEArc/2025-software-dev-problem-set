d00c6f89fad93e0bab3bd6bbebb8ef16579bca5a
SeanEArc <seanearcaya@gmail.com>
2025-09-17 14:58:43 -0500

    Completed LeetCode Problem Set 111_Container_With_ Most_Water.js

---
+++ solutions/11_Container_With_Most_Water.js
@@ -0,0 +1,37 @@
+// 11. Container With Most Water ... Solution.
+/* 
+PERSONAL NOTE:
+We attempted the brute force approach; however, we ran into a time limit exceeded error. We then researched the two-pointer approach and implemented it.
+*/
+
+
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function(height) {
+
+    let left = 0;
+    let right = height.length - 1;
+
+    let maxArea = 0;
+
+    while (left < right) {
+
+        let width = right - left;
+        let h = Math.min(height[left], height[right]);
+
+        let area = width * h;
+        maxArea = Math.max(maxArea, area);
+
+        if (height[left] < height[right]) {
+            left++;
+        } else {
+            right--;
+        }
+
+    }
+
+    return maxArea;
+    
+};
